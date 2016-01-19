1d251721207ccd3efe64631929aab5c46e9d1a9f
Sandro Ciervo <Sandro Ciervo>
2016-01-19 12:48:16 +0100

    Use SQLite for Project. Created a class to manage database

---
+++ scripts/schema.sql
@@ -0,0 +1,55 @@
+-- noinspection SqlDialectInspectionForFile
+DROP TABLE IF EXISTS accounts;
+CREATE TABLE accounts (
+    id   INTEGER     PRIMARY KEY AUTOINCREMENT,
+    type INTEGER,    NOT NULL,
+    name STRING (80) NOT NULL
+);
+
+DROP TABLE IF EXISTS locations;
+CREATE TABLE locations (
+    id                  INTEGER     PRIMARY KEY AUTOINCREMENT,
+    street              STRING (80) NOT NULL,
+    zipCode             STRING (10) NOT NULL,
+    city                STRING (50) NOT NULL,
+    amountParkingSpaces INTEGER
+);
+
+DROP TABLE IF EXISTS members;
+CREATE TABLE members (
+    id                   INTEGER      PRIMARY KEY AUTOINCREMENT,
+    accountId            INTEGER      REFERENCES accounts (id),
+    name                 STRING (80)  NOT NULL,
+    postalAddress        STRING (350) NOT NULL,
+    defaultLocationId    INTEGER      REFERENCES locations (id),
+    emailAddress         STRING (180) UNIQUE NOT NULL,
+    pinCode              STRING (6)   NOT NULL,
+    drivingLicenceNumber STRING (30)  UNIQUE NOT NULL,
+    homePhoneNumber      STRING (20),
+    mobilePhoneNumber    STRING (20),
+    businessPhoneNumber  STRING (20),
+    faxPhoneNumber       STRING (20) 
+);
+
+DROP TABLE IF EXISTS carCategories;
+CREATE TABLE carCategories (
+    id               INTEGER      PRIMARY KEY AUTOINCREMENT,
+    name             STRING (25)  NOT NULL,
+    grundausstattung STRING (400) 
+);
+
+DROP TABLE IF EXISTS car;
+CREATE TABLE car (
+    id                    INTEGER     PRIMARY KEY AUTOINCREMENT,
+    parkingSpotLocationId INTEGER     REFERENCES locations (id),
+    carCategoryId         INTEGER     REFERENCES carCategories (id),
+    plateNumber           STRING (20) NOT NULL
+);
+
+DROP TABLE IF EXISTS reservations;
+CREATE TABLE reservations (
+    id        INTEGER  PRIMARY KEY AUTOINCREMENT,
+    carId     INTEGER  REFERENCES car (id),
+    startDate DATETIME NOT NULL,
+    endDate   DATETIME NOT NULL
+);
---
+++ src/Database.java
@@ -0,0 +1,162 @@
+import java.io.File;
+import java.util.ArrayList;
+
+import com.almworks.sqlite4java.SQLiteConnection;
+import com.almworks.sqlite4java.SQLiteConstants;
+import com.almworks.sqlite4java.SQLiteException;
+import com.almworks.sqlite4java.SQLiteStatement;
+
+public class Database implements AutoCloseable {
+    private SQLiteConnection db;
+    private String delimiter = "[,]";
+
+    private static final String DB_PATH = System.getProperty("user.home") + "\\" + "leihauto.db";
+
+    public Database() throws SQLiteException {
+        db = new SQLiteConnection(new File(DB_PATH));
+        db.open(true);
+    }
+
+    @Override
+    public void close() throws Exception {
+        if (db != null && !db.isDisposed()) {
+            db.dispose();
+        }
+    }
+
+    public boolean execute(String query) throws SQLiteException {
+        SQLiteStatement stmt = db.prepare(query);
+        boolean result = false;
+        try {
+            if (stmt.step()) {
+                result = true;
+            }
+        } finally {
+            stmt.dispose();
+        }
+        return result;
+    }
+
+    public String firstString(String query) throws SQLiteException {
+        return firstString(query, null);
+    }
+
+    public String firstString(String query, String args) throws SQLiteException {
+        SQLiteStatement stmt = db.prepare(query + " LIMIT 1");
+        String value = null;
+        try {
+            if (args != null) {
+                String[] parameters = args.split(delimiter);
+                for (int i = 0; i < parameters.length; i++) {
+                    stmt.bind(i + 1, parameters[i]);
+                }
+            }
+            if (stmt.step()) {
+                value = stmt.columnString(0);
+            }
+        } finally {
+            stmt.dispose();
+        }
+        return value;
+    }
+
+    public String selectSingle(String query, String args) throws SQLiteException {
+        SQLiteStatement st = db.prepare(query);
+        String[] parameters = args.split(delimiter);
+        String result = null;
+        try {
+            for (int i = 0; i < parameters.length; i++) {
+                st.bind(i + 1, parameters[i]);
+            }
+            int columnsCount = st.columnCount();
+            if (columnsCount > 1) {
+                System.err.println("Error: More than a single column!");
+                return null;
+            }
+
+            if (st.step()) {
+                int type = st.columnType(0);
+
+                switch (type) {
+                    case SQLiteConstants.SQLITE_INTEGER:
+                        result = String.valueOf(st.columnInt(0));
+                    case SQLiteConstants.SQLITE_TEXT:
+                        result = st.columnString(0);
+                    default:
+                        break;
+                }
+            }
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        } finally {
+            st.dispose();
+        }
+        return result;
+    }
+
+    public ArrayList selectMany(String query, String args) {
+        ArrayList result = new ArrayList();
+        SQLiteStatement st = null;
+        try {
+            st = db.prepare(query);
+            String[] parameters;
+            if (!args.isEmpty() && args != null) {
+                parameters = args.split(delimiter);
+                for (int i = 0; i < parameters.length; i++) {
+                    st.bind(i + 1, parameters[i]);
+                }
+            }
+
+            int columnsCount = st.columnCount();
+            if (columnsCount > 1) {
+                System.err.println("Error: More than a single column!");
+                return null;
+            }
+            while (st.step()) {
+                int type = st.columnType(0);
+
+                switch (type) {
+                    case SQLiteConstants.SQLITE_INTEGER:
+                        result.add(String.valueOf(st.columnInt(0)));
+                        break;
+
+                    case SQLiteConstants.SQLITE_TEXT:
+                        result.add(st.columnString(0));
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        } finally {
+            st.dispose();
+        }
+        return result;
+    }
+
+    public boolean updateSingleField(String query, String args) {
+        boolean success = false;
+        SQLiteStatement st = null;
+        try {
+            db.exec("BEGIN TRANSACTION; ");
+            st = db.prepare(query);
+            String[] parameters = args.split(delimiter);
+
+            for (int i = 0; i < parameters.length; i++) {
+                st.bind(i + 1, parameters[i]);
+            }
+            st.step();
+            db.exec("COMMIT;");
+            success = true;
+        } catch (SQLiteException e) {
+            System.err.println(e.getMessage());
+        } finally {
+            if (st != null) {
+                st.dispose();
+            }
+        }
+        return success;
+    }
+}
