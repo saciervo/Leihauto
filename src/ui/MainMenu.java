332adce39a4c6eaca911cf7b2a8436cf0bca71c0
Sandro Ciervo <Sandro Ciervo>
2016-02-02 15:13:25 +0100

    Entry point of the application is now in the MainMenu

--- src/ui/MainMenu.java
+++ src/ui/MainMenu.java
@@ -1,22 +1,31 @@
 package ui;
 
+import infrastructure.sqlite.init.DatabaseInitializer;
+
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class MainMenu {
     private JPanel panel;
     private JButton newReservationButton;
     private JButton findReservationButton;
+    private JButton resetDatabaseButton;
 
     public MainMenu() {
         newReservationButton.addActionListener(e -> CreateReservation.show());
         findReservationButton.addActionListener(e -> FindReservation.show());
+        resetDatabaseButton.addActionListener(e -> DatabaseInitializer.init());
     }
 
-    public static void show() {
-        JFrame frame = new JFrame("Leihauto");
+    public static void main(String[] args) {
+        Logger.getLogger("com.almworks.sqlite4java").setLevel(Level.OFF);
+
+        JFrame frame = new JFrame("MainMenu");
         frame.setContentPane(new MainMenu().panel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
     }
